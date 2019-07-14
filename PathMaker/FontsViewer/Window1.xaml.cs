567eeda279ddfe8e67be98007dcca9ef0aa37605
Yosuke Maeda <yosuke_maeda@hotmail.com>
2019-07-14 13:40:54 +0900

    1.0

---
+++ PathMaker/FontsViewer/Window1.xaml.cs
@@ -0,0 +1,252 @@
+﻿// $Header: /ComSpexHome.root/ComSpexHome/Visual Studio 2010/Projects/ComSpexHome/FontsViewer/Window1.xaml.cs 1     11/07/13 7:23p Yosuke $
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+
+namespace FontsViewer {
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1:Window {
+		ICollection<FontFamily> fontFamilies;
+		public Window1() {
+			InitializeComponent();
+			fontFamilies=Fonts.SystemFontFamilies;
+			this.unifog.Columns=4;
+			this.unifog.Rows=fontFamilies.Count/this.unifog.Columns;
+			if(fontFamilies.Count%this.unifog.Columns!=0) {
+				++this.unifog.Rows;
+			}
+			foreach(FontFamily fontFamily in fontFamilies) {
+				Report("FontFamily.Source={0}",fontFamily.Source);
+				string[] familyName=fontFamily.Source.Split('#');
+				string name=familyName[familyName.Length-1];
+				this.fontList.Items.Add(name);
+			}
+			DispatcherTimer Loader=new DispatcherTimer();
+			Loader.Interval=TimeSpan.FromSeconds(0.5);
+			Loader.Tick+=new EventHandler(Loader_Tick);
+			Loader.Start();
+		}
+		void Loader_Tick(object sender,EventArgs e) {
+			statusText.Content=String.Format("{0}","Just moment, please...");
+			statusText.Foreground=Brushes.White;
+			
+			this.Opacity=0.5;
+			scroller.IsEnabled=false;
+			((DispatcherTimer)sender).Stop();
+			this.Cursor=Cursors.AppStarting;
+			int index=0;
+			double diff=0.5/Convert.ToDouble(fontFamilies.Count);
+			foreach(FontFamily fontFamily in fontFamilies) {
+				string[] familyName=fontFamily.Source.Split('#');
+				string name=familyName[familyName.Length-1];
+				FontFamily ff=new FontFamily(name);
+				StackPanel child=new StackPanel();
+				child.RenderTransformOrigin=new Point(0.5,0.5);
+				{
+					TextBlock block=new TextBlock();
+					block.FontFamily=ff;
+					block.Text=(string)this.Resources["alphaUpper"];
+					child.Children.Add(block);
+				}
+				{
+					TextBlock block=new TextBlock();
+					block.FontFamily=ff;
+					block.Text=(string)this.Resources["alphaLower"];
+					child.Children.Add(block);
+				}
+				{
+					TextBlock block=new TextBlock();
+					block.FontFamily=ff;
+					block.Text=(string)this.Resources["numbers"];
+					child.Children.Add(block);
+				}
+				{
+					TextBlock block=new TextBlock();
+					block.FontFamily=ff;
+					block.Text=(string)this.Resources["symbols"];
+					child.Children.Add(block);
+				}
+				{
+					TextBlock block=new TextBlock();
+					block.FontFamily=ff;
+					block.Text=(string)this.Resources["japanese"];
+					child.Children.Add(block);
+				}
+				{
+					TextBlock block=new TextBlock();
+					block.FontFamily=ff;
+					Binding bind=new Binding();
+					bind.Source=samplet;
+					bind.Path=new PropertyPath(TextBlock.TextProperty);
+					block.SetBinding(TextBlock.TextProperty,bind);
+					child.Children.Add(block);
+				}
+				Border inner=new Border();
+				inner.Tag=index;
+				inner.Style=this.Resources["fontInner"] as Style;
+				inner.Child=child;
+
+				Border outer=new Border();
+				outer.Tag=index;
+				outer.Style=this.Resources["fontOuter"] as Style;
+				outer.MouseDown+=new MouseButtonEventHandler(SetViewboxMaximized);
+				outer.PreviewMouseMove+=new MouseEventHandler(UpdateStatusBar);
+				outer.Child=inner;
+				
+				unifog.Children.Add(outer);
+
+				++index;
+				DoEvents();
+				this.Opacity+=diff;
+				this.Title=String.Format("System Font Family - Loading {0,3}/{1} {2}",index,fontFamilies.Count,name);
+			}
+			this.Cursor=Cursors.Arrow;
+			this.fontList.SelectedIndex=0;
+			scroller.IsEnabled=true;
+			this.Opacity=1.0;
+			statusText.Content=String.Format("{0}","Ready");
+			statusText.Foreground=Brushes.Blue;
+		}
+		void UpdateStatusBar(object sender,MouseEventArgs e) {
+			Border outer=sender as Border;
+			if(outer!=null){
+				statusText.Content=String.Format("{0}",fontList.Items[(int)outer.Tag]);
+				statusText.Foreground=Brushes.Black;
+			}
+		}
+		void DoEvents() {
+			DispatcherFrame frame=new DispatcherFrame();
+			Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Render,new DispatcherOperationCallback(ExitFrames),frame);
+			Dispatcher.PushFrame(frame);
+		}
+		object ExitFrames(object f) {
+			((DispatcherFrame)f).Continue=false;
+			return null;
+		}
+		private void SetViewboxMaximized(object sender,MouseButtonEventArgs e) {
+			Border outer=sender as Border;
+			Border inner=outer.Child as Border;
+			if(inner!=null){
+				inner.CaptureMouse();
+				outer.Child=null;
+				viewSpecimen.Child=inner;
+				inner.BorderBrush=new SolidColorBrush(Color.FromRgb(0x33,0x99,0xff));
+				fontList.SelectedIndex=(int)outer.Tag;
+				fontList.ScrollIntoView(fontList.SelectedItem);
+			}
+		}
+		private void Viewbox_MouseDown(object sender,MouseButtonEventArgs e) {
+			if(e.LeftButton==MouseButtonState.Pressed) {
+				SetViewboxRestored();
+			}
+		}
+		private void Viewbox_KeyDown(object sender,KeyEventArgs e) {
+			if(e.Key==Key.Escape&&e.IsDown) {
+				SetViewboxRestored();
+			}
+		}
+		private void SetViewboxRestored(){
+			FrameworkElement specimen=viewSpecimen.Child as FrameworkElement;
+			Border inner=specimen as Border;
+			if(inner!=null){
+				inner.ReleaseMouseCapture();
+				viewSpecimen.Child=null;
+				Border outer=unifog.Children[(int)inner.Tag] as Border;
+				outer.Child=inner;
+			}
+		}
+		void Report(string format,params object[] args) {
+			System.Diagnostics.Debug.WriteLine(String.Format(format,args),DateTime.Now.ToString("HH:mm:ss.fff"));
+		}
+		List<Border> pools=new List<Border>();
+		private void fontList_SelectionChanged(object sender,SelectionChangedEventArgs e) {
+			string familyName=(string)fontList.SelectedItem;
+			this.Title=String.Format("System Font Family - {0}",familyName);
+			bool isChild=false;
+			string key=String.Empty;
+			string[] cmds=Environment.GetCommandLineArgs();
+			if(cmds.Length==2){
+				string cmd=cmds[1];
+				if(cmd.ToLower().StartsWith("/key:")){
+					key=cmd.Substring(cmd.IndexOf(':')+1);
+					if(!String.IsNullOrEmpty(key)){
+						isChild=true;
+					}
+				}
+			}
+			if(isChild){
+				Clipboard.SetText(String.Format("{0}={1}",key,familyName));
+			} else {
+				Clipboard.SetText(familyName);
+			}
+			if(unifog.Children.Count>=fontFamilies.Count){
+				foreach(Border pool in pools){
+					Border inner=(pool.Child as Border);
+					if(inner!=null){
+						inner.BorderBrush=Brushes.Transparent;
+					}
+				} 
+				pools.Clear();
+				Border outer=unifog.Children[fontList.SelectedIndex] as Border;
+				if(outer!=null){
+					if(outer.Child!=null){
+						Border inner=outer.Child as Border;
+						if(inner!=null){
+							inner.BorderBrush=new SolidColorBrush(Color.FromRgb(0x33,0x99,0xff));
+							outer.BringIntoView();
+							pools.Add(outer);
+						}
+					}
+				}
+			}
+		}
+		private void MenuItem_Click(object sender,RoutedEventArgs e) {
+			MenuItem mitem=sender as MenuItem;
+			switch((string)mitem.Header){
+				case "Italic":
+					if(mitem.IsChecked) {
+						viewSpecimen.SetValue(TextBlock.FontStyleProperty,FontStyles.Normal);
+					} else {
+						viewSpecimen.SetValue(TextBlock.FontStyleProperty,FontStyles.Italic);
+					}
+					break;
+				case "Bold":
+					if(mitem.IsChecked) {
+						viewSpecimen.SetValue(TextBlock.FontWeightProperty,FontWeights.Normal);
+					} else {
+						viewSpecimen.SetValue(TextBlock.FontWeightProperty,FontWeights.Bold);
+					}
+					break;
+				default:
+					viewSpecimen.SetValue(TextBlock.FontStyleProperty,FontStyles.Normal);
+					viewSpecimen.SetValue(TextBlock.FontWeightProperty,FontWeights.Normal);
+					doItalic.IsChecked=doBold.IsChecked=false;
+					break;
+			}
+			mitem.IsChecked=!mitem.IsChecked;
+			doClear.IsEnabled=(doItalic.IsChecked||doBold.IsChecked);
+			doClear.IsChecked=false;
+		}
+		private void samplet_MouseDown(object sender,MouseButtonEventArgs e) {
+			InputBox Box=new InputBox(samplet,this);
+			Box.ShowDialog();
+		}
+		private void Window_PreviewKeyDown(object sender,KeyEventArgs e) {
+			Viewbox_KeyDown(sender,e);
+		}
+	}
+}
---
+++ PathMaker/OutlinedText/OutlinedText.cs
@@ -0,0 +1,351 @@
+﻿// $Header: /Greetings.root/Greetings/OutlinedText/OutlinedText.cs 14    13/02/02 16:29 Yosuke $
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Xml;
+using System.IO;
+using System.Windows.Markup;
+using System.Reflection;
+
+namespace ComSpex {
+	/// <summary>
+	/// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+	///
+	/// Step 1a) Using this custom control in a XAML file that exists in the current project.
+	/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+	/// to be used:
+	///
+	///     xmlns:MyNamespace="clr-namespace:OutlinedText"
+	///
+	///
+	/// Step 1b) Using this custom control in a XAML file that exists in a different project.
+	/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+	/// to be used:
+	///
+	///     xmlns:MyNamespace="clr-namespace:OutlinedText;assembly=OutlinedText"
+	///
+	/// You will also need to add a project reference from the project where the XAML file lives
+	/// to this project and Rebuild to avoid compilation errors:
+	///
+	///     Right click on the target project in the Solution Explorer and
+	///     "Add Reference"->"Projects"->[Select this project]
+	///
+	///
+	/// Step 2)
+	/// Go ahead and use your control in the XAML file.
+	///
+	///     <MyNamespace:CustomControl1/>
+	///
+	/// </summary>
+	public class OutlinedText:Control {
+		public static readonly DependencyProperty FillProperty;
+		public static readonly DependencyProperty HighlightProperty;
+		public static readonly DependencyProperty StrokeProperty;
+		public static readonly DependencyProperty StrokeThicknessProperty;
+		public static readonly DependencyProperty TextProperty;
+		public static readonly DependencyProperty CultureProperty;
+		static OutlinedText() {
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(OutlinedText),new FrameworkPropertyMetadata(typeof(OutlinedText)));
+			{
+				FrameworkPropertyMetadata meta=new FrameworkPropertyMetadata();
+				meta.DefaultValue=Brushes.Transparent;
+				meta.AffectsMeasure=false;
+				meta.AffectsRender=true;
+				meta.Inherits=true;
+				meta.DefaultUpdateSourceTrigger=UpdateSourceTrigger.PropertyChanged;
+				FillProperty=DependencyProperty.Register("Fill",typeof(Brush),typeof(OutlinedText),meta);
+			}
+			HighlightProperty=DependencyProperty.Register("Highlight",typeof(bool),typeof(OutlinedText));
+			{
+				FrameworkPropertyMetadata meta=new FrameworkPropertyMetadata();
+				meta.DefaultValue=Brushes.Black;
+				meta.AffectsMeasure=
+				meta.AffectsRender=true;
+				meta.Inherits=true;
+				meta.DefaultUpdateSourceTrigger=UpdateSourceTrigger.PropertyChanged;
+				StrokeProperty=DependencyProperty.Register("Stroke",typeof(Brush),typeof(OutlinedText),meta);
+			}
+			{
+				FrameworkPropertyMetadata meta=new FrameworkPropertyMetadata();
+				meta.DefaultValue=1.0;
+				meta.AffectsMeasure=
+				meta.AffectsRender=true;
+				meta.Inherits=true;
+				meta.DefaultUpdateSourceTrigger=UpdateSourceTrigger.PropertyChanged;
+				StrokeThicknessProperty=DependencyProperty.Register("StrokeThickness",typeof(double),typeof(OutlinedText),meta);
+			}
+			{
+				FrameworkPropertyMetadata meta=new FrameworkPropertyMetadata();
+				meta.DefaultValue="Outlined Text";
+				meta.AffectsMeasure=
+				meta.AffectsRender=true;
+				meta.Inherits=true;
+				meta.DefaultUpdateSourceTrigger=UpdateSourceTrigger.PropertyChanged;
+				TextProperty=DependencyProperty.Register("Text",typeof(string),typeof(OutlinedText),meta);
+			}
+			{
+				FrameworkPropertyMetadata meta=new FrameworkPropertyMetadata();
+				meta.DefaultValue="en-US";
+				meta.AffectsMeasure=false;
+				meta.AffectsRender=true;
+				meta.Inherits=true;
+				meta.DefaultUpdateSourceTrigger=UpdateSourceTrigger.PropertyChanged;
+				CultureProperty=DependencyProperty.Register("Culture",typeof(string),typeof(OutlinedText),meta);
+			}
+		}
+		
+		// Class variables
+		private FormattedText _formattedText;
+
+		// Property variables
+		private Geometry _textGeometry;
+		private Geometry _textHighLightGeometry;
+
+		public OutlinedText(){
+		}
+		public OutlinedText(string textContent) {
+			// Set the default property values.
+			FontFamily=new FontFamily("Verdana");
+			FontStretch=FontStretches.Normal;
+			FontWeight=FontWeights.Bold;
+			FontSize=72;
+			Text=textContent;
+		}
+
+		// Create the outline geometry based on the formatted text.
+		public void CreateText() {
+			// Create the formatted text based on the properties set.
+			_formattedText=new FormattedText(
+					Text,
+					CultureInfo.GetCultureInfo(Culture),
+					FlowDirection.LeftToRight,
+					new Typeface(FontFamily,FontStyle,FontWeight,FontStretch),
+					FontSize,
+					Brushes.Black // This brush does not matter since we use the geometry of the text. 
+					);
+
+			// Build the geometry object that represents the text.
+			_textGeometry=_formattedText.BuildGeometry(new Point(0,0));
+
+			// Build the geometry object that represents the text hightlight.
+			if(Highlight) {
+				_textHighLightGeometry=_formattedText.BuildHighlightGeometry(new Point(0,0));
+			}
+		}
+
+		// The OnRender override method allows you to draw directly into
+		// the DrawingContext of the outlined text control.
+		protected override void OnRender(DrawingContext drawingContext) {
+			// Draw the outline based on the properties that are set.
+			drawingContext.DrawGeometry(Fill,new Pen(Stroke,StrokeThickness),_textGeometry);
+
+			// Draw the text highlight based on the properties that are set.
+			if(Highlight) {
+				drawingContext.DrawGeometry(Fill,new Pen(Stroke,StrokeThickness),_textHighLightGeometry);
+			}
+		}
+
+		// The ArrangeOverride and MeasureOverride methods provide layout support for the control.
+		//  - ArrangeOverride: Positions the object and determines size. 
+		//  - MeasureOverride: Measures and determines the size in layout required for the object.
+
+		// No calculation required -- return the value of the parameter.
+		protected override Size ArrangeOverride(Size finalSize) {
+			return finalSize;
+		}
+
+		// Return the size of the formatted text.
+		protected override Size MeasureOverride(Size availableSize) {
+			CreateText();
+			return new Size(_formattedText.Width,_formattedText.Height);
+		}
+
+		// Definitions for the outlined text control properties.
+		public Brush Fill {
+			get { return (Brush)GetValue(FillProperty); }
+			set { SetValue(FillProperty,value); }
+		}
+		public bool Highlight {
+			get { return (bool)GetValue(HighlightProperty);}
+			set { SetValue(HighlightProperty,value);}
+		}
+		public Brush Stroke {
+			get { return (Brush)GetValue(StrokeProperty);}
+			set { SetValue(StrokeProperty,value);}
+		}
+		public double StrokeThickness {
+			get { return (double)GetValue(StrokeThicknessProperty);}
+			set { SetValue(StrokeThicknessProperty,value);}
+		}
+		public string Text {
+			get { return (string)GetValue(TextProperty);}
+			set { SetValue(TextProperty,value);}
+		}
+		public string Culture{
+			get{return (string)GetValue(CultureProperty);}
+			set{SetValue(CultureProperty,value);}
+		}
+		// The TextGeometry property allows you to get and set the geometry of the outlined text.
+		// The returned value can be used to clip images, video, etc.
+		public Geometry TextGeometry {
+			get {
+				CreateText();
+				return _textGeometry;
+			}
+			set { _textGeometry=value; }
+		}
+		protected virtual string filterSymbols(string text) {
+			Dictionary<string,string> dics=new Dictionary<string,string>();
+			dics.Add("~","Tilda");
+			dics.Add("!","Excla");
+			dics.Add("@","Atmar");
+			dics.Add("#","Hashi");
+			dics.Add("$","Dolla");
+			dics.Add("%","Perce");
+			dics.Add("^","Caret");
+			dics.Add("&","Amper");
+			dics.Add("*","Aster");
+			dics.Add("(","LPare");
+			dics.Add(")","RPare");
+			dics.Add("_","Under");
+			dics.Add("-","Minus");
+			dics.Add("+","Pluss");
+			dics.Add("=","Equal");
+			dics.Add("{","LBrac");
+			dics.Add("}","RBrac");
+			dics.Add("[","LSBra");
+			dics.Add("]","RSBra");
+			dics.Add("|","Verti");
+			dics.Add("\\","Backs");
+			dics.Add(":","Colon");
+			dics.Add(";","Semic");
+			dics.Add("\"","Quote");
+			dics.Add("'","Singl");
+			dics.Add("<","LAngl");
+			dics.Add(">","RAngl");
+			dics.Add(",","Comma");
+			dics.Add(".","Perio");
+			dics.Add("?","Quest");
+			dics.Add("/","Slash");
+			foreach(KeyValuePair<string,string> pair in dics) {
+				text=text.Replace(pair.Key,pair.Value);
+			}
+			return text;
+		}
+		protected virtual string XamlForSilverlight{
+			get{
+				OutlinedText textRequested=this;
+				string text=XamlWriter.Save(textRequested);
+				text=text.Replace("av:","");
+				XmlDocument xd=new XmlDocument();
+				xd.LoadXml(text);
+				XmlNodeList data=xd.GetElementsByTagName("PathGeometry");
+				using(StringWriter sw=new StringWriter()) {
+					string textName=filterSymbols(textRequested.Text.Replace(" ",""));
+					sw.WriteLine("<Grid>");
+					sw.WriteLine("<Grid.Resources>");
+					sw.WriteLine(@"<Style TargetType=""Path"" x:Key=""{0}"">",textName);
+					sw.WriteLine(@"<Setter Property=""Fill"" Value=""{0}"" />",textRequested.Fill);
+					sw.WriteLine(@"<Setter Property=""Stroke"" Value=""{0}"" />",textRequested.Stroke);
+					sw.WriteLine(@"<Setter Property=""StrokeThickness"" Value=""{0}"" />",textRequested.StrokeThickness);
+					sw.WriteLine(@"</Style>");
+					for(int i=0;i<data.Count;++i) {
+						sw.WriteLine(@"<Style TargetType=""Path"" x:Key=""style_{0}{2}"" BasedOn=""{{StaticResource {1}}}""></Style>",textName[i],textName,i);
+					}
+					sw.WriteLine("</Grid.Resources>");
+					for(int i=0;i<data.Count;++i) {
+						XmlNode node=data[i];
+						XmlAttribute xatt=node.Attributes["Figures"];
+						sw.WriteLine(@"<Path Style=""{{StaticResource style_{0}{2}}}"" Data=""{1}"" />",textName[i],xatt.Value,i);
+					}
+					sw.WriteLine(@"<Grid.RenderTransform>");
+					sw.WriteLine(@"<TransformGroup>");
+					sw.WriteLine(@"<TranslateTransform X=""0"" Y=""0"" />");
+					sw.WriteLine(@"</TransformGroup>");
+					sw.WriteLine(@"</Grid.RenderTransform>");
+					sw.WriteLine("</Grid>");
+					return sw.ToString();
+				}
+			}
+		}
+		protected virtual string XamlForWPF{
+			get{
+				OutlinedText textRequested=this;
+				string text=XamlWriter.Save(textRequested);
+				text=text.Replace("av:","");
+				XmlDocument xd=new XmlDocument();
+				xd.LoadXml(text);
+				XmlNode data=xd.GetElementsByTagName("OutlinedText.TextGeometry")[0];
+				using(StringWriter sw=new StringWriter()) {
+					string xaml=data.InnerXml;
+					xaml=xaml.Replace(String.Format(@" xmlns=""{0}""",data.NamespaceURI),"");
+					sw.WriteLine("<!-- {0} -->",textRequested.Text);
+					sw.WriteLine(@"<Path Fill=""{0}"" Stroke=""{1}"" StrokeThickness=""{2}"">",textRequested.Fill,textRequested.Stroke,textRequested.StrokeThickness);
+					sw.WriteLine("<Path.Data>{0}</Path.Data>",xaml);
+					sw.WriteLine("</Path>");
+					return sw.ToString();
+				}
+			}
+		}
+		public string ColorByName(Brush brush){
+			if(brush is SolidColorBrush){
+				SolidColorBrush rhs=brush as SolidColorBrush;
+				Type type=typeof(Brushes);
+				PropertyInfo[] props=type.GetProperties(BindingFlags.Static|BindingFlags.Public);
+				foreach(PropertyInfo prop in props){
+					//System.Diagnostics.Debug.WriteLine(prop.Name);
+					SolidColorBrush lhs=prop.GetValue(prop,null) as SolidColorBrush;
+					if(rhs.Color==lhs.Color){
+						return prop.Name;
+					}
+				}
+			}
+			return brush.ToString();
+		}
+		public override string ToString() {
+			//<OutlinedText Fill="#FFF5F5F5" Stroke="#FF000000" Text="White Smoke" xmlns="clr-namespace:ComSpex;assembly=OutlinedText" />
+			string text=String.Empty;
+			using(StringWriter sw=new StringWriter()){
+				sw.Write(@" Fill=""{0}""",ColorByName(this.Fill));
+				sw.Write(@" Stroke=""{0}""",ColorByName(this.Stroke));
+				sw.Write(@" StrokeThickness=""{0}""",this.StrokeThickness);
+				sw.Write(@" FontSize=""{0}""",this.FontSize);
+				sw.Write(@" FontWeight=""{0}""",this.FontWeight);
+				sw.Write(@" FontStretch=""{0}""",this.FontStretch);
+				sw.Write(@" FontStyle=""{0}""",this.FontStyle);
+				sw.Write(@" FontFamily=""{0}""",this.FontFamily);
+				sw.Write(@" Text=""{0}""",this.Text);
+				string xaml=XamlWriter.Save(this);
+				xaml=xaml.Replace("av:","");
+				XmlDocument xd=new XmlDocument();
+				xd.LoadXml(xaml);
+				XmlNode data=xd.GetElementsByTagName("OutlinedText.TextGeometry")[0];
+				sw.Write(@" xmlns=""{0}""",data.NamespaceURI);
+				text=String.Format("<OutlinedText {0} />\n",sw.ToString());
+			}
+			return text;
+		}
+		public virtual string ToXAML(bool isWpf){
+			string xaml=String.Empty;
+			if(isWpf){
+				xaml=XamlForWPF;
+			}else{
+				xaml=XamlForSilverlight;
+			}
+			return xaml;
+		}
+		public string ToXAML() {
+			return ToXAML(true);
+		}
+	}
+}
